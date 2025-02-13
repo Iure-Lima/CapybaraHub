008ac7c8c93c6d616dab904fd97ee96875f61204
IureLima <iure.lima@jala.university>
2025-02-12 23:14:26 -0300

    feat: add the feature for register in the system

--- src/app/services/auth/register.service.ts
+++ src/app/services/auth/register.service.ts
@@ -2,16 +2,18 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
+import { UserRegister } from '../../models/user.register.model';
 
 @Injectable({
   providedIn: 'root'
 })
-export class LoginService {
-
-  constructor(private http: HttpClient) { }
+export class RegisterService {
+  constructor(private http: HttpClient){}
 
   // biome-ignore lint/suspicious/noExplicitAny: <explanation>
-  login(email: string, password: string): Observable<any> {
-    return this.http.post(`${environment.apiUrl}/auth/signin`, { email, password })
+  registerUser(user: UserRegister): Observable<any> {
+    console.log(user)
+    return this.http.post(`${environment.apiUrl}/auth/signup`, user)
   }
+
 }
