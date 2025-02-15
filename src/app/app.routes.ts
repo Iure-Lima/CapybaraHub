9e9fc40cd7b3c4b375f7c8b92c867bc5902cf2e9
IureLima <iure.lima@jala.university>
2025-02-15 18:30:58 -0300

    feat: configuration the router for the reservations

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import type { Routes } from '@angular/router';
 import { CardListComponent } from './page/card-list/card-list.component';
-import { NotFoundComponent } from './page/not-found/not-found.component';
+import { ReservationsComponent } from './page/reservations/reservations.component';
 
 export const routes: Routes = [
   {path:"home", component: CardListComponent},
+  {path: "reservations/:id", component: ReservationsComponent},
   {path:"", redirectTo:"home", pathMatch:"full"},
-  {path:"**", component: NotFoundComponent}
+  {path:"**", loadComponent: () => import("../app/page/not-found/not-found.component").then(m => m.NotFoundComponent)}
 ];
