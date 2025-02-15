abb8d327f2c9cf09129b5ed660845e0604170bd6
IureLima <iure.lima@jala.university>
2025-02-15 20:00:51 -0300

    feat: create the main style for reservations

--- src/app/page/reservations/reservations.component.ts
+++ src/app/page/reservations/reservations.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CalendarModule } from 'primeng/calendar';
+import { DropdownModule } from 'primeng/dropdown';
+import { FloatLabelModule } from 'primeng/floatlabel';
 
 @Component({
   selector: 'app-reservations',
   standalone: true,
-  imports: [],
+  imports: [CalendarModule, FormsModule, DropdownModule, FloatLabelModule],
   templateUrl: './reservations.component.html',
   styleUrl: './reservations.component.css'
 })
 export class ReservationsComponent {
+  selectedDates: Date[] = []
+  guestOptions: string[] = ["Teste","Teste2"]
+  selectedGuests!:string;
+  disabledDates: Date[] = [new Date()];
+  today: Date = new Date();
 
 }
