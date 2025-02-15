71874d8cb09f4e53b044f8c4ed6f46ec04b035c5
IureLima <iure.lima@jala.university>
2025-02-14 21:52:37 -0300

    feat: create the service for hotel

--- src/app/services/hotel/hotel.service.ts
+++ src/app/services/hotel/hotel.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+import { HotelCard } from '../../models/hotel.card.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HotelService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAllHotels():Observable<HotelCard[]>{
+    return this.http.get<HotelCard[]>(`${environment.apiUrl}/hotels`)
+  }
+
+  getHotelById(id: string): Observable<HotelCard>{
+    return this.http.get<HotelCard>(`${environment.apiUrl}/hotels/${id}`)
+  }
 }
