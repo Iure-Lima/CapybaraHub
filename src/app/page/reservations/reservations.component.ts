20346d033d92ac857094627b684fe8bb084fcce2
IureLima <iure.lima@jala.university>
2025-02-16 11:20:00 -0300

    feat: configuration the integration on prices

--- src/app/page/reservations/reservations.component.ts
+++ src/app/page/reservations/reservations.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, DoCheck, Input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CalendarModule } from 'primeng/calendar';
@@ -13,25 +14,54 @@ import { RoomService } from '../../services/room/room.service';
 @Component({
   selector: 'app-reservations',
   standalone: true,
-  imports: [CalendarModule, FormsModule, DropdownModule, FloatLabelModule],
+  imports: [CalendarModule, FormsModule, DropdownModule, FloatLabelModule, NgClass],
   templateUrl: './reservations.component.html',
   styleUrl: './reservations.component.css'
 })
-export class ReservationsComponent {
-  selectedDates: Date[] = []
-  guestOptions: string[] = ["Teste","Teste2"]
+export class ReservationsComponent implements DoCheck {
+  @Input() selectedDates: Date[] = []
+  guestOptions: string[] = ["1 Guest","2 Guest"]
   selectedGuests!:string;
+  totalPriceWithNights = 0
+  totalPrice = 0;
+  totalNights = 0;
   disabledDates: Date[] = []; //Aqui mais vai entrar os dados referentes as datas com agendamento
   today: Date = new Date();
 
-  room!: RoomCard;
-  hotel!: HotelCard;
+  //Tanto room quanto hotel iniciam com propriedades vazias, para evitar erros
+  room: RoomCard = {
+    _id: '',
+    hotel: '',
+    roomTypeId: '',
+    pricePerNight: '',
+    roomNumber: 0,
+    status: 'Available',
+    images: [],
+    rating: 0
+  };
+  hotel: HotelCard = {
+    _id: '',
+    name: '',
+    email: '',
+    phone: '',
+    rating: 0,
+    address: {
+      street: '',
+      city: '',
+      state: '',
+      country: '',
+      number: 0,
+      postalCode: ''
+    },
+    image: ''
+  };
 
   constructor(private roomService: RoomService, private hotelService: HotelService, private route : ActivatedRoute, private alertService: AlertService, private router: Router){
     // biome-ignore lint/complexity/useLiteralKeys: <explanation>
     this.roomService.getRoomById(this.route.snapshot.params['id']).subscribe({
       next: (response) => {
         this.room = response;
+        this.totalPrice = Number(this.room.pricePerNight);
 
         //Vai definir o hotel quando a requisição do room for um sucesso
         this.hotelService.getHotelById(this.room.hotel).subscribe({
@@ -57,6 +87,23 @@ export class ReservationsComponent {
         this.router.navigate(['/home'])
       }
     })
+  }
+  ngDoCheck(): void {
+    if (!(this.selectedDates.length < 2)) {
+      if (this.selectedDates[0] && this.selectedDates[1]){
+        this.totalNights = this.getNumberOfDays(this.selectedDates[0], this.selectedDates[1]);
+        this.totalPriceWithNights = Number(this.room.pricePerNight) * this.totalNights;
+      }else{
+        this.totalNights = 1;
+        this.totalPriceWithNights = Number(this.room.pricePerNight);
+      }
+    }
+    this.totalPrice = this.totalPriceWithNights + 185
+  }
+
+  getNumberOfDays(startDate: Date, endDate: Date): number {
+    const diffInMs = endDate.getTime() - startDate.getTime();
+    return diffInMs / (1000 * 60 * 60 * 24) + 1;
   }
 
 }
