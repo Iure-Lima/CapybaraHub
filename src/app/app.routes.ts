7e43e9a57de04abbc261b9d26886ac461c26face
IureLima <iure.lima@jala.university>
2025-02-16 13:33:14 -0300

    feat: create the guard for auth the user

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import type { Routes } from '@angular/router';
+import { authGuard } from './guard/auth.guard';
 import { CardListComponent } from './page/card-list/card-list.component';
 
 export const routes: Routes = [
   {path:"home", component: CardListComponent},
   {path: "reservations/:id", loadComponent: () => import("./page/reservations/reservations.component").then(m => m.ReservationsComponent)},
-  {path:"booking/:id", loadComponent: () => import('./page/booking/booking.component').then(m => m.BookingComponent)},
+  {path:"booking/:id", loadComponent: () => import('./page/booking/booking.component').then(m => m.BookingComponent), canActivate:[authGuard]},
   {path:"", redirectTo:"home", pathMatch:"full"},
   {path:"**", loadComponent: () => import("../app/page/not-found/not-found.component").then(m => m.NotFoundComponent)}
 ];
