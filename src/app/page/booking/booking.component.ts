0146cac4d54dd302d2a96dad96f561fe17208126
IureLima <iure.lima@jala.university>
2025-02-16 12:40:29 -0300

    refactor: modidy the static values for values of the reservations

--- src/app/page/booking/booking.component.ts
+++ src/app/page/booking/booking.component.ts
@@ -15,10 +15,9 @@ export class BookingComponent {
 
   constructor(private cacheBookingService: CacheBookingService, private router: Router, private route: ActivatedRoute){
     this.cacheDatas = this.cacheBookingService.getDataCache()
-    console.log(this.cacheDatas)
     if (!this.cacheDatas){
       // biome-ignore lint/complexity/useLiteralKeys: <explanation>
-      this.router.navigate(['/reservations', this.route.snapshot.params['id']])
+      // this.router.navigate(['/reservations', this.route.snapshot.params['id']])
     }
   }
 
