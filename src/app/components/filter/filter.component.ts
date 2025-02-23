880fde535d955accb659ef25186ebac3913679d7
IureLima <iurelimasilva@gmail.com>
2025-02-23 00:12:16 -0300

    feat: add the rooms and beds section in the component filter

--- src/app/components/filter/filter.component.ts
+++ src/app/components/filter/filter.component.ts
@@ -2,14 +2,14 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { DialogModule } from 'primeng/dialog';
+import { DropdownModule } from 'primeng/dropdown';
 import { SliderModule } from 'primeng/slider';
-
 
 
 @Component({
   selector: 'app-filter',
   standalone: true,
-  imports: [FormsModule, DialogModule,SliderModule, CommonModule],
+  imports: [FormsModule, DialogModule,SliderModule, CommonModule,DropdownModule],
   templateUrl: './filter.component.html',
   styleUrl: './filter.component.css'
 })
@@ -18,6 +18,12 @@ export class FilterComponent{
   rangeValues: number[] = [69, 1560];
   minPrice = this.rangeValues[0]
   maxPrice = this.rangeValues[1]
+
+  //Esses dados vão vir da api de um modelo que ainda esta em desenvolvimento
+  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+  roomsAndBeds: any[] = [{name:"Teste", code:1},{name:"Teste1",code:1},{name:"Test2",code:1}]
+  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+  selectedRoomsAndBeds: any | undefined = this.roomsAndBeds[0];
 
   showFilterDialog(){
     this.visible = !this.visible;
