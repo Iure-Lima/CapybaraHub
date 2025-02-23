f57014391e65f3ad4ac2953962ee7e258107cd4a
IureLima <iurelimasilva@gmail.com>
2025-02-23 00:45:25 -0300

    feat: add and configure the calendar section

--- src/app/components/filter/filter.component.ts
+++ src/app/components/filter/filter.component.ts
@@ -1,29 +1,43 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CalendarModule } from 'primeng/calendar';
 import { DialogModule } from 'primeng/dialog';
 import { DropdownModule } from 'primeng/dropdown';
+import { FloatLabelModule } from 'primeng/floatlabel';
 import { SliderModule } from 'primeng/slider';
+
 
 
 @Component({
   selector: 'app-filter',
   standalone: true,
-  imports: [FormsModule, DialogModule,SliderModule, CommonModule,DropdownModule],
+  imports: [FormsModule, DialogModule,SliderModule, CommonModule,DropdownModule,CalendarModule,FloatLabelModule],
   templateUrl: './filter.component.html',
   styleUrl: './filter.component.css'
 })
-export class FilterComponent{
+export class FilterComponent implements OnInit{
   visible = true;
   rangeValues: number[] = [69, 1560];
   minPrice = this.rangeValues[0]
   maxPrice = this.rangeValues[1]
+
+  //Calendar data
+  @Input() selectedDates: Date[] = [];
+  minDate: Date = new Date();
+  disabledDates: Date[] = [];
+
 
   //Esses dados vão vir da api de um modelo que ainda esta em desenvolvimento
   // biome-ignore lint/suspicious/noExplicitAny: <explanation>
   roomsAndBeds: any[] = [{name:"Teste", code:1},{name:"Teste1",code:1},{name:"Test2",code:1}]
   // biome-ignore lint/suspicious/noExplicitAny: <explanation>
   selectedRoomsAndBeds: any | undefined = this.roomsAndBeds[0];
+
+  ngOnInit(): void {
+    this.minDate = new Date(this.minDate);
+    this.minDate.setDate(this.minDate.getDate() + 1);
+  }
 
   showFilterDialog(){
     this.visible = !this.visible;
@@ -38,4 +52,5 @@ export class FilterComponent{
       this.maxPrice = event.values[1];
     }
   }
+
 }
