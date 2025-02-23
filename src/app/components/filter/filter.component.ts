63786d2f456bd7c5268ba670d948143705f32a8a
IureLima <iurelimasilva@gmail.com>
2025-02-22 21:15:10 -0300

    feat: create the price range

--- src/app/components/filter/filter.component.ts
+++ src/app/components/filter/filter.component.ts
@@ -1,20 +1,35 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { DialogModule } from 'primeng/dialog';
+import { SliderModule } from 'primeng/slider';
+
+
 
 @Component({
   selector: 'app-filter',
   standalone: true,
-  imports: [CommonModule, DialogModule],
+  imports: [FormsModule, DialogModule,SliderModule, CommonModule],
   templateUrl: './filter.component.html',
   styleUrl: './filter.component.css'
 })
-export class FilterComponent {
-  visible = false;
-  position = 'center';
-
+export class FilterComponent{
+  visible = true;
+  rangeValues: number[] = [69, 1560];
+  minPrice = this.rangeValues[0]
+  maxPrice = this.rangeValues[1]
 
   showFilterDialog(){
     this.visible = !this.visible;
+  }
+
+  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+  handleRangeChange(event: any) {
+    if (event.values[0] < this.maxPrice){
+      this.minPrice = event.values[0];
+    }
+    if (event.values[1] > this.minPrice){
+      this.maxPrice = event.values[1];
+    }
   }
 }
