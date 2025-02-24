cfd29dd17bca162226b94acb6ff256f1aacc2d84
IureLima <iurelimasilva@gmail.com>
2025-02-24 15:30:04 -0300

    feat: add the carousel primeng component

--- src/app/components/hotel-list/hotel-card/hotel-card.component.ts
+++ src/app/components/hotel-list/hotel-card/hotel-card.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { AvatarModule } from 'primeng/avatar';
+
 
 @Component({
   selector: 'app-hotel-card',
   standalone: true,
-  imports: [],
+  imports: [AvatarModule],
   templateUrl: './hotel-card.component.html',
   styleUrl: './hotel-card.component.css'
 })
--- src/app/components/hotel-list/hotel-list.component.ts
+++ src/app/components/hotel-list/hotel-list.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
+import { CarouselModule } from 'primeng/carousel';
+import { HotelCard } from '../../models/hotel.card.model';
 import { HotelCardComponent } from "./hotel-card/hotel-card.component";
 
 @Component({
   selector: 'app-hotel-list',
   standalone: true,
-  imports: [HotelCardComponent],
+  imports: [HotelCardComponent,CarouselModule],
   templateUrl: './hotel-list.component.html',
   styleUrl: './hotel-list.component.css'
 })
 export class HotelListComponent {
-
+  hotels: HotelCard[] = [];
+  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+  responsiveOptions: any[] | undefined;
 }
