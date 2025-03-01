1e60ff9801f08e614075bb3e4d86c80ce31fc985
IureLima <iurelimasilva@gmail.com>
2025-03-01 19:19:38 -0300

    refactor: add the onpush strategy in the footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
   imports: [],
   templateUrl: './footer.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class FooterComponent {}
